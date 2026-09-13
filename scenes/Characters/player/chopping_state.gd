970bd4bac68740411bd8b928f92e0433cd882b96
Zadow67 <officialwarmthful@gmail.com>
2026-09-14 02:17:51 +0530

    fixed hit range also added rocks and stones ...

--- scenes/Characters/player/chopping_state.gd
+++ scenes/Characters/player/chopping_state.gd
@@ -25,7 +25,7 @@ func _on_enter() -> void:
 		hit_component_collision_shape.position = Vector2(7,-19)
 	elif player.player_direction == Vector2.DOWN:
 		animated_sprite_2d.play("chopping_front")
-		hit_component_collision_shape.position = Vector2(7,19)
+		hit_component_collision_shape.position = Vector2(1,3)
 	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("chopping_right")
 		hit_component_collision_shape.position = Vector2(13,0)
